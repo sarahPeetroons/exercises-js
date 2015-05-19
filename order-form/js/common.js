ebb1e0e5f361fea1968f817cecc280d61c45fa0d
Sarah peetroons <sps@emakina.com>
2015-05-19 17:54:52 +0200

    check "first" number (+-)

--- order-form/js/common.js
+++ order-form/js/common.js
@@ -4,7 +4,7 @@
 	var numberTest = function(){
 		$("#card").keydown(function (e) {
 
-			console.log($("#card").val());
+			//console.log($("#card").val());
 
 	        // Allow: backspace, delete, tab, escape, enter and .
 	        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
@@ -22,10 +22,28 @@
 	    });
 	};
 
+	var checkFirstNumber = function(){
+		/*var regVisa = /^4[0-9]{12}(?:[0-9]{3})?$/;
+        var regMasterCard = /^5[1-5][0-9]{14}$/;*/
+        
+        $("#card").keydown(function (e) {
+        	var value = $(this).val() + 1;
+        	if(value.indexOf(4) == 0){
+	        	console.log('visa');
+	        	$(this).parent().find('.input-group-addon').empty().append('visa');
+	        }else if(value.indexOf(5) == 0){
+        		console.log('masterCard');
+	        	$(this).parent().find('.input-group-addon').empty().append('masterCard');
+	        }else{
+	        	$(this).parent().find('.input-group-addon').empty();
+	        }
+	    });
+	};
 	
 
 	$(document).ready(function() {
 		numberTest();
+		checkFirstNumber();
 	});
 
 })(jQuery);
